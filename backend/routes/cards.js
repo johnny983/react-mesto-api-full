d6fa952da9e43b03057a0375e2568aa46475bd37
REFRESH <refresh@johnny.local>
2020-11-29 21:07:19 +0300

    remove cors

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -1,30 +1,11 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
+// const { celebrate, Joi } = require('celebrate');
 const { getCards, createCard, deleteCard } = require('../controllers/cards');
 
-router.get('/', celebrate({
-  headers: Joi.object().keys({
-    Authorization: Joi.string().token(),
-  }).unknown(true),
-}), getCards);
+router.get('/', getCards);
 
-router.post('/', celebrate({
-  headers: Joi.object().keys({
-    Authorization: Joi.string().token(),
-  }).unknown(true),
-  body: Joi.object().keys({
-    name: Joi.string(),
-    link: Joi.string().regex(/^https?:\/\/[\w*-?./]*\/?$/i),
-  }),
-}), createCard);
+router.post('/', createCard);
 
-router.delete('/:cardId', celebrate({
-  headers: Joi.object().keys({
-    Authorization: Joi.string().token(),
-  }).unknown(true),
-  params: Joi.object().keys({
-    id: Joi.string().alphanum().length(24),
-  }),
-}), deleteCard);
+router.delete('/:cardId', deleteCard);
 
 module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,52 +1,15 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
+// const { celebrate, Joi } = require('celebrate');
 const {
   login, createUser, getCurrentUser, getUsers, getUserById, changeUserInfo, changeAvatar,
 } = require('../controllers/users');
 
-router.post('/signup', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().email().min(7),
-    password: Joi.string(),
-  }),
-}), createUser);
-router.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().email().min(7),
-    password: Joi.string(),
-  }),
-}), login);
-router.get('/me', celebrate({
-  headers: Joi.object().keys({
-    Authorization: Joi.string().token(),
-  }).unknown(true),
-}), getCurrentUser);
-router.get('/:id', celebrate({
-  params: Joi.object().keys({
-    id: Joi.string().alphanum().length(24),
-  }),
-}), getUserById);
-router.get('/', celebrate({
-  headers: Joi.object().keys({
-    Authorization: Joi.string().token(),
-  }).unknown(true),
-}), getUsers);
-router.patch('/me', celebrate({
-  headers: Joi.object().keys({
-    Authorization: Joi.string().token(),
-  }).unknown(true),
-  body: Joi.object().keys({
-    name: Joi.string(),
-    about: Joi.string(),
-  }),
-}), changeUserInfo);
-router.patch('/me/avatar', celebrate({
-  headers: Joi.object().keys({
-    Authorization: Joi.string().token(),
-  }).unknown(true),
-  body: Joi.object().keys({
-    avatar: Joi.string().regex(/^https?:\/\/[\w*-?./]*\/?$/i),
-  }),
-}), changeAvatar);
+router.post('/signup', createUser);
+router.post('/signin', login);
+router.get('/me', getCurrentUser);
+router.get('/:id', getUserById);
+router.get('/', getUsers);
+router.patch('/me', changeUserInfo);
+router.patch('/me/avatar', changeAvatar);
 
 module.exports = router;
